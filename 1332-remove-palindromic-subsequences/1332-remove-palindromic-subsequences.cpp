d51407d95b751cd8d6286fdc524bc207a02eb322
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-08 10:55:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,25 +2,10 @@ class Solution {
 public:
     int removePalindromeSub(string s) 
     {
-        //if the given string is empty return 0
-        if(s.size()==0)
-        { 
-            return 0;
-        }
-        
-        int i=0, j=s.size()-1;
-        while(i<j) 
-        {
-			//If the given string is not palindrome return 2
-            if(s[i]!=s[j])
-            { 
-                return 2;
-            }
-            i++;
-            j--;
-        }
-        
-        return 1;
+        string t=s;
+        reverse(t.begin(),t.end());
+        if(t == s) return 1;
+        return 2;
     }
 };
         
