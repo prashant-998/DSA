2538f5b9dac106c517af638b234b69e751729c96
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-13 11:26:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (91.22%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
+    int game(int n,int k)
+    {
+        //Base Case
+        if (n == 1)
+        {
+            return 0;
+        }
+        else 
+        {
+            //circluar round 
+            return(game(n-1,k)+k)%n;
+        }
+    }
     int findTheWinner(int n, int k) 
     {
-        //initialized the queue
-        queue<int> q;
-        
-        //pushed the elements into the queue
-        for(int i=1;i<=n;i++)
-        {
-            q.push(i);
-        }
-        
-        //loop will run when queue!=1
-        //if it is 1 we will return it
-        while(q.size()!=1)
-        {
-            for(int i=1;i<k;i++)
-            {
-                //element ouccuring before that element
-                int t=q.front();
-                //poped from the front 
-                q.pop();
-                //pused at the last
-                q.push(t);
-            }
-            q.pop();
-        }
-        return q.front();
-        
+        //indexing incresed by one
+        return game(n,k)+1;
     }
 };
