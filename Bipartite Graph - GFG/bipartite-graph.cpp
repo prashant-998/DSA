5fe4bb85396fdc98ccb08558e9e0adf55a75db68
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-17 23:59:16 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -7,78 +7,51 @@ class Solution
 {
 public:
 	bool DFSisBipartite(int node, vector<int> adj[], vector<bool> &visited, vector<int> &color)
-
-    {
-
-        visited[node] = true;
-
-        
-
-        for (auto nbr : adj[node])
-
-        {
-
-            if (!visited[nbr])
-
-            {
-
-                color[nbr] = !color[node];      //child ka color parent se alag hoga 
-
-                if (!DFSisBipartite(nbr, adj, visited, color))
-
-                    return false;
-
-            }
-
-            else
-
-            {
-
-                if (node != nbr && color[node] == color[nbr])       //agar baap aur bete ka color same he matlab Graph is not Bipartite 
-
-                    return false;
-
-            }
-
-        }
-
-        return true;
-
-    }
-
-    
-
- 
-
-bool isBipartite(int V, vector<int>adj[]){
-
-   
-
-   vector<bool> visited(V , 0) ;   
-
-   vector<int> color(V , 0) ;      //ye node ka color store karega 
-
-   
-
-   for(int i=0 ; i<V ; i++)
-
-   {
-
-       if( !visited[i] )
-
-       {
-
-           if( !DFSisBipartite( i , adj , visited , color) ) 
-
-               return false ; 
-
-       }
-
-   }
-
-   return true ; 
-
-}
+	{
+	    visited[node] = true;
+	    for (auto nbr : adj[node])
+	    {
+	        if (!visited[nbr])
+	        {
+	            color[nbr] = !color[node];//child ka color parent se alag hoga 
+	            if (!DFSisBipartite(nbr, adj, visited, color))
+	            {
+	                return false;
+	            }
+	            
+	        }
+	        else
+	        {
+	            if (node != nbr && color[node] == color[nbr]) //agar baap aur bete ka color same he matlab Graph is not Bipartite 
+	            {
+	                return false;
+	            }
+	            
+	        }
+	        
+	    }
+	    return true;
+	    
+	}
+	bool isBipartite(int V, vector<int>adj[])
+	{
+	    vector<bool> visited(V , 0) ;   
+	    vector<int> color(V , 0) ;//ye node ka color store karega 
+	    for(int i=0 ; i<V ; i++)
+	    {
+	        if( !visited[i] )
+	        {
+	            if( !DFSisBipartite( i , adj , visited , color)) 
+	            {
+	                return false ;
+	            }
+	            
+	        }
+	        
+	    }
+	    return true ; 
+	    
+	}
 };
 
 // { Driver Code Starts.
