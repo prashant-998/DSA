86cd9ff93cd5057ff9c6fcfe909fbd6498324a17
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-20 22:34:24 +0530

    Added solution - LeetHub

--- Number of positive integral solutions - GFG/number-of-positive-integral-solutions.cpp
+++ Number of positive integral solutions - GFG/number-of-positive-integral-solutions.cpp
@@ -9,30 +9,32 @@ class Solution
 public:
     long posIntSol(string s)
     {
-        int n=0,r=0;
-        for(int i=0; i<s.length(); i++)
+        int n = 0,r = 0;
+        
+        for(int i = 0; i<s.length(); i++)
         {
-            if(s[i]=='+')
+            if(s[i] == '+')
             {
                 r++;
             }
             
-            if(s[i]=='=')
+            if(s[i] == '=')
             {
-                n=stoll(s.substr(i+1));
+                n = stoll(s.substr(i+1));
             }
         }
         r++;
-         n = n-1;
-         r = r-1;
+        n = n-1;
+        r = r-1;
          
-        long long val1 =1, val2=1;
-        for(long long i =n; i>n-r; i--)
+        long long val1 = 1, val2 = 1;
+        
+        for(long long i = n; i>n-r; i--)
         {
             val1 *= i;
         }
         
-        for(long long i =1; i<=r; i++)
+        for(long long i = 1; i<=r; i++)
         {
             val2 *= i;
         }
