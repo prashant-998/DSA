1b7629ffa524469f378f94373f87b536ea803310
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-21 12:55:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.56%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,42 @@
+#define mod 1000000007
+class Solution 
+{
+public:
+    long long solve(long long x, long long n)
+    {
+        //base condition
+        if(n == 0)
+        {
+            return 1;
+        }
+        long long ans = solve(x, n/2);
+        
+        if(n&1)
+        {
+            ans = ans*ans*x;
+            ans = ans % mod;
+        }
+        else
+        {
+            ans = ans*ans;
+            ans = ans%mod;
+        }
+        return ans;
+    }
+    
+    int countGoodNumbers(long long n) 
+    {
+        //long long oddPow =;
+        long long res;
+        if(n&1)
+        {
+            res = (solve(5, n/2+1)*solve(4, n/2))%mod;
+        }
+        else
+        {
+            res = (solve(5, n/2)*solve(4, n/2))%mod;
+        }
+        
+        return res;
+    }
+};
