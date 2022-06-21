d972683e8a4e596133f0596a996be99caa279e2a
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-21 13:02:19 +0530

    Time: 201 ms (14.99%), Space: 6.1 MB (49.25%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) 
+    {
+        string s="";
+        for(int i = 1;i<=n;i++)
+        {
+            s+=to_string(i);
+        }
+        
+        // we used k>1 not k==0 because we have 
+        // already created 1st so we need k-1th permutation.
+        while(k>1)
+        {
+            // this funtion is used to find next permutation
+            next_permutation(s.begin(),s.end());
+            k--;
+        }
+        return s;
+    }
+};
