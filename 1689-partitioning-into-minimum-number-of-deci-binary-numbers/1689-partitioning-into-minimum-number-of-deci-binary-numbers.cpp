e4e76955feb3f68c260587971665da5e11fb1fbc
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-06-27 22:50:49 +0530

    Time: 70 ms (11.27%), Space: 13.6 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,18 +1,26 @@
+// class Solution 
+// {
+// public:
+//     int minPartitions(string n) 
+//     {
+//         int len = n.length();
+//         int max_digit_from_string = 0;
+        
+//         //The auto keyword specifies that the type of the variable that is being declared will be automatically deducted from its initializer. 
+//         for(auto element:n)
+//         {
+//             //convert the string number into integer by substracting the ascii value.
+//             int digit = element-'0';
+//             max_digit_from_string = max(max_digit_from_string, digit);
+//         }
+//         return max_digit_from_string;
+//     }
+// };
 class Solution 
 {
 public:
     int minPartitions(string n) 
     {
-        int len = n.length();
-        int max_digit_from_string = 0;
-        
-        //The auto keyword specifies that the type of the variable that is being declared will be automatically deducted from its initializer. 
-        for(auto element:n)
-        {
-            //convert the string number into integer by substracting the ascii value.
-            int digit = element-'0';
-            max_digit_from_string = max(max_digit_from_string, digit);
-        }
-        return max_digit_from_string;
+        return *max_element(n.begin(), n.end()) - '0';
     }
 };
