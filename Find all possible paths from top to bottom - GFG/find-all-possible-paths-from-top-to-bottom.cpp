75f75499061ea898c84129acf0421092c10cabfd
prashant-998 <90751921+prashant-998@users.noreply.github.com>
2022-07-10 14:22:35 +0530

    Added solution - LeetHub

---
+++ Find all possible paths from top to bottom - GFG/find-all-possible-paths-from-top-to-bottom.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public: 
+vector<vector<int>>ans;
+    vector<int>temp;
+    void xxx(int i,int j,int n,int m,vector<vector<int>>&grid)
+    {
+        if(i==n-1 && j==m-1)
+        {
+            temp.push_back(grid[i][j]);
+            ans.push_back(temp);
+            temp.pop_back();
+            return;
+        }
+        
+        temp.push_back(grid[i][j]);
+        if(i!=n-1)
+        xxx(i+1,j,n,m,grid);
+        if(j!=m-1)
+        xxx(i,j+1,n,m,grid);
+        temp.pop_back();
+    }
+    vector<vector<int>> findAllPossiblePaths(int n, int m, vector<vector<int>> &grid)
+    {
+        ans.clear();
+        temp.clear();
+        xxx(0,0,n,m,grid);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> grid[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.findAllPossiblePaths(n, m, grid);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            for (int j = 0; j < ans[i].size(); j++)
+            {
+                cout << ans[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
+  // } Driver Code Ends
